8aa934c3e83f4d551dc81efd19d5c6b508ad5e87
Leo-64 <leoelplz@gmail.com>
2023-04-05 20:05:53 +0200

    added Class.cp

---
+++ assets/Class.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+class PersonalClass {
+public:
+	void print() {
+		std::cout << "Hello i am Personal Class!" << std::endl;
+	}
+};
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "assets/Class.cpp"
 
 int main() {
 	std::cout << "Hello you all" << std::endl;
-
+	PersonalClass p_class = PersonalClass();
+	p_class.print();
 }
